cec96dc0b4d5d166189212ed4ce5cd843879c40a
llgoer <llgoer@github.com>
2020-05-09 11:20:31 +0800

    增加Gin运行示例

---
+++ examples/gin/main.go
@@ -0,0 +1,37 @@
+// Copyright 2020 The GoKeep Authors. All rights reserved.
+// license that can be found in the LICENSE file.
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	gktpl "github.com/gokeeptech/gktemplate"
+	"log"
+	"os"
+)
+
+const CTYPE = "text/html; charset=utf-8"
+
+func init() {
+	// 开发模式下启用日志调试
+	if os.Getenv("GKENV") != "dev" {
+		gin.SetMode(gin.ReleaseMode)
+	}
+}
+
+func main() {
+	r := gin.Default()
+	// 加载模板
+	gktpl.LoadDir("./templates/*.htm")
+	r.GET("/", func(c *gin.Context) {
+		data := gktpl.D{
+			"info": "Template engine for GoKeep(GK)，GoKeep模板引擎",
+		}
+		// 渲染模板
+		rs, err := gktpl.Parse("templates/simple.htm", data)
+		if err != nil {
+			log.Println(err)
+		}
+		c.Data(200, CTYPE, []byte(rs))
+	})
+	r.Run()
+}
